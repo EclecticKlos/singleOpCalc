55456af1e10f2f4db51f51974b53183abd849c9a
Dan Klos <dsklos@gmail.com>
2016-06-01 08:25:55 -0700

    Initial Commit

---
+++ firstCPPProgram/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  firstCPPProgram
+//
+//  Created by Dan Klos on 6/1/16.
+//  Copyright © 2016 Dan Klos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
