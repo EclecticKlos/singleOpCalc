dd54bb37979300cc631b5032350396b1f31a0c61
Dan Klos <dsklos@gmail.com>
2016-10-07 23:30:48 -0700

    Starting over

--- firstCPPProgram/main.cpp
+++ firstCPPProgram/main.cpp
@@ -7,9 +7,55 @@
 //
 
 #include <iostream>
+#include <map>
+#include <vector>
+#include <string.h>
+#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+using namespace std;
+
+bool isOperator(char x)
+{
+    if(strncmp( &x, "*",1) == 0 || strncmp( &x, "/",1) == 0 || strncmp( &x, "+",1) == 0 || strncmp( &x, "-",1) == 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+int main()
+{
+    
+    
+//////////////////////// NOTES ////////////////////////
+
+//  Likely would be best to read directly from the stream and determine each character as either
+//  string or num?
+
+//  Should break each algorithm manipulation into separate function
+    
+//  Learn what "&" is actually doing
+    
+///////////////////////////////////////////////////////
+    vector<string> numsInAlgo;
+    vector<char> tempWorkingAlgo;
+    
+    cout << "Let's do some math! Tell me what you want to do without spaces. \n";
+    string userInput = "";
+    getline(cin, userInput);
+    vector<char> usersAlgorithm;
+    for(int i=0; i<userInput.length(); i++){
+        if(isdigit(userInput[i])){
+            
+        }
+        usersAlgorithm.push_back(userInput[i]);
+    }
+    
+    // Must have two seperate data structures, one for nums one for operators
+    
+ 
+    
+    
 }
